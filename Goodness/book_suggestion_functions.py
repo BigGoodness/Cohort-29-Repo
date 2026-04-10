181720008c2ac1a45073658c80937df6686f50e7
crazyminerparody <wsholanky@gmail.com>
2026-04-10 10:45:34 +0100

    Add folder

--- Goodness/book_suggestion_functions.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestCase
-import book_suggestion_system
-
-class book_suggestion_functions(TestCase):
-    def test_for_show_all_books(self):
-        books = [The Hobbit, The Mystery, Brave Kind]
--- Goodness/book_suggestion_functions_testing.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-import book_suggestion_system
-
-class book_suggestion_functions(TestCase):
-    def test_for_show_all_books(self):
-        books = ["The Hobbit," "The Mystery," "Brave Kind"]
-        actual = book_suggestion_system.suggest_books(books)
-        expected = 1
-        self.assertEqual(actual, expected)
