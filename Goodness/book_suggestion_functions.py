c6e113ce0b64562fe44fd14e1cd8c6fcaed76475
crazyminerparody <wsholanky@gmail.com>
2026-04-10 10:49:20 +0100

    Goodness

---
+++ Goodness/book_suggestion_functions.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+import book_suggestion_system
+
+class book_suggestion_functions(TestCase):
+    def test_for_show_all_books(self):
+        books = [The Hobbit, The Mystery, Brave Kind]
---
+++ Goodness/book_suggestion_system.py
@@ -0,0 +1,31 @@
+import random
+books = ["The Hobbit", "The Mystery", "Brave kind"]
+
+def suggest_books(books):
+        random_book = random.choice(books)
+        random_page = random.randint(1, 100)
+        return random_book, random_page
+
+
+def add_book(books, book_name):
+    if book_name not in books:
+        books.append(book_name)
+        return True
+
+
+def remove_book(books, book_name):
+    if book_name in books:
+        books.remove(book_name)
+        return True
+    return False
+
+def update_book(books, old_book_name, new_book_name):
+    if old_book_name in books:
+        index = books.index(old_book_name)
+        books[index] = new_book_name
+        return True
+    return False
+
+
+def show_all_books(books):
+    return books
